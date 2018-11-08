9a78f8de18d38782054e45ea94ebcfbbd809083a
Hassan Shahbazi <h-shahbazi@hotmail.com>
2018-11-08 14:04:16 +0330

    Weighted Quick Union with path compression

--- Quick_Union/src/com/coursera/Main.java
+++ Quick_Union/src/com/coursera/Main.java
@@ -37,8 +37,10 @@ class Quick_Union {
     }
 
     private int root(int i) {
-        while (i != id[i])
+        while (i != id[i]) {
+            id[i] = id[id[i]];
             i = id[i];
+        }
         return i;
     }
 
